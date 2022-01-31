353dcd0f6881c0427df75bca817b17ddd6b5e86a
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-30 16:29:57 -0800

    Deploying

--- server/server.js
+++ server/server.js
@@ -1,25 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
@@ -52,9 +30,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/booksearch-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
